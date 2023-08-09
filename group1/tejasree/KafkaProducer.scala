3c0d1620197f4f58ba7fa826db292d73a3d5e5c9
TejasreePai <100678360+TejasreePai@users.noreply.github.com>
2023-08-09 11:53:38 +0530

    Update KafkaProducer.scala

--- group1/tejasree/KafkaProducer.scala
+++ group1/tejasree/KafkaProducer.scala
@@ -8,22 +8,27 @@ object KafkaProducer {
 
   def main(args: Array[String]): Unit = {
 
-    val props: Properties = new Properties();
-    props.put("bootstrap.servers", "localhost:9092");
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-    val producer = new KafkaProducer[String, String](props)
 
     val TOPIC = "datastreaming"
-    val filename="C:/Users/Dell/IdeaProjects/datastreaming_team1/targetReadyDataSet.csv/targetReadyDataSet.csv"
+    val filename = "C:/Users/Dell/IdeaProjects/datastreaming_team1/targetReadyDataSet.csv/targetReadyDataSet.csv"
+    produce(filename,TOPIC)
 
-    for(line <- Source.fromFile(filename).getLines()) {
-
-      val record = new ProducerRecord[String,String](TOPIC,"key",line)
-      producer.send(record)
-    }
-    producer.flush()
-    producer.close()
   }
+    def produce(filename: String, TOPIC: String): Unit = {
+
+      val props: Properties = new Properties();
+      props.put("bootstrap.servers", "localhost:9092");
+      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+      val producer = new KafkaProducer[String, String](props)
+      for (line <- Source.fromFile(filename).getLines()) {
+
+        val record = new ProducerRecord[String, String](TOPIC, "key", line)
+        producer.send(record)
+      }
+      producer.flush()
+      producer.close()
+    }
+
 }
