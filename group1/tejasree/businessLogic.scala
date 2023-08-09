f465e079e4ede2cd532a4c5895653b85348db939
TejasreePai <100678360+TejasreePai@users.noreply.github.com>
2023-08-09 11:52:50 +0530

    Update businessLogic.scala

--- group1/tejasree/businessLogic.scala
+++ group1/tejasree/businessLogic.scala
@@ -7,26 +7,33 @@ object bussinessLogic{
     val sc = spark.sparkContext
     sc.setLogLevel("ERROR")
 
+    val password = "thejasree"
+
     val dfRaw = spark.read.format("com.databricks.spark.csv")
       .option("delimiter", ",")
       .load("C:/Users/Dell/IdeaProjects/datastreaming_team1/ConsumerData.csv")
 
     val colum_names = Seq("product_id", "location_id", "selling_channel", "prod_description", "retail_price", "onhand_quantity", "create_date", "promotion_eligibility")
     val df = dfRaw.toDF(colum_names: _*)
+    val validDF = implementLogic(df,"error_selling_channel","error_retail_price", "error_product_id",password)
+    validDF.show(false)
+  }
 
-    val errorChannel = df.filter((df("selling_channel")=!="Cross Over" && df("selling_channel")=!="Store Only"  && df("selling_channel")=!="Online Only"))
+    def implementLogic(df:DataFrame,errchannel:String,errprice:String,errproductId:String,password:String): DataFrame ={
+      val errorChannel = df.filter((df("selling_channel") =!= "Cross Over" && df("selling_channel") =!= "Store Only" && df("selling_channel") =!= "Online Only"))
 
-    errorChannel.write
-      .format("jdbc")
-      .option("driver","com.mysql.cj.jdbc.Driver")
-      .option("url","jdbc:mysql://localhost:3306/datastreaming")
-      .option("dbtable", "error_selling_channel")
-      .option("user", "root")
-      .option("password", "thejasree")
-      .mode("overwrite")
-      .save()
+      errorChannel.write
+        .format("jdbc")
+        .option("driver", "com.mysql.cj.jdbc.Driver")
+        .option("url", "jdbc:mysql://localhost:3306/datastreaming")
+        .option("dbtable", errchannel)
+        .option("user", "root")
+        .option("password", password)
+        .mode("overwrite")
+        .save()
 
-    val validChannels= df.filter((df("selling_channel")==="Cross Over" || df("selling_channel")==="Store Only"  || df("selling_channel")==="Online Only"))
+
+      val validChannels = df.filter((df("selling_channel") === "Cross Over" || df("selling_channel") === "Store Only" || df("selling_channel") === "Online Only"))
 
     val errorPrice = validChannels.filter(validChannels("retail_price")===0 || validChannels("retail_price").isNull)
 
@@ -34,15 +41,29 @@ object bussinessLogic{
       .format("jdbc")
       .option("driver","com.mysql.cj.jdbc.Driver")
       .option("url","jdbc:mysql://localhost:3306/datastreaming")
-      .option("dbtable", "error_retail_price")
+      .option("dbtable", errprice)
       .option("user", "root")
-      .option("password", "thejasree")
+      .option("password", password)
       .mode("overwrite")
       .save()
 
+
     val removeNullPrice= validChannels.filter(validChannels("retail_price")=!=0 && validChannels("retail_price").isNotNull)
 
-    val validProductId = removeNullPrice.filter(removeNullPrice.col("product_id").cast("int").isNotNull)
+    val productIdDf = removeNullPrice.filter(removeNullPrice.col("product_id").cast("int").isNotNull)
+
+    val validProductId = productIdDf.filter(length(col("product_id")) === 8)
+    val errorProductId = productIdDf.filter(length(col("product_id")) =!= 8)
+
+    errorProductId.write
+      .format("jdbc")
+      .option("driver","com.mysql.cj.jdbc.Driver")
+      .option("url","jdbc:mysql://localhost:3306/datastreaming")
+      .option("dbtable", errproductId)
+      .option("user", "root")
+      .option("password", password)
+      .mode("overwrite")
+      .save()
 
     val deduplicatedDF = validProductId.dropDuplicates(Seq("product_id", "location_id"))
 
@@ -54,7 +75,7 @@ object bussinessLogic{
       .option("url","jdbc:mysql://localhost:3306/datastreaming")
       .option("dbtable", "online_selling_channel")
       .option("user", "root")
-      .option("password", "thejasree")
+      .option("password",password)
       .mode("overwrite")
       .save()
 
@@ -66,7 +87,7 @@ object bussinessLogic{
       .option("url","jdbc:mysql://localhost:3306/datastreaming")
       .option("dbtable", "crossover_selling_channel")
       .option("user", "root")
-      .option("password", "thejasree")
+      .option("password", password)
       .mode("overwrite")
       .save()
 
@@ -79,10 +100,11 @@ object bussinessLogic{
       .option("url","jdbc:mysql://localhost:3306/datastreaming")
       .option("dbtable", "store_selling_channel")
       .option("user", "root")
-      .option("password", "thejasree")
+      .option("password", password)
       .mode("overwrite")
       .save()
 
+      deduplicatedDF
 
   }
 }
