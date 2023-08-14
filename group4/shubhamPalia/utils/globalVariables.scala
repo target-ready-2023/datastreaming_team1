504c3a546a60e9cb513855319a17cc7f4c4337ab
shubham <shubhampalia26@gmail.com>
2023-08-14 22:41:56 +0530

    Update globalVariables.scala

--- group4/shubhamPalia/utils/globalVariables.scala
+++ group4/shubhamPalia/utils/globalVariables.scala
@@ -2,4 +2,8 @@ package utils
 
 object globalVariables {
   val kafkaTopic: String = "datastreaming_database"
+  val databaseURL: String = "jdbc:mysql://localhost:3306/targetReadyDataBase"
+  val userName = "root"
+  val password: String = "targetReady@2023"
+
 }
